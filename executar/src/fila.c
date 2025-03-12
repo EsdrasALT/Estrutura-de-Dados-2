6d5f3c016c917f7c0e5a81b4c7ca2a6076c2b57e
EsdrasALT <esdras.a.pessoal@gmail.com>
2025-03-11 22:11:49 -0300

    update

---
+++ executar/src/fila.c
@@ -0,0 +1,50 @@
+#include "fila.h"
+
+void cria (FILA *fila){
+	fila->fim=(PONT)malloc(sizeof(CELULA));
+	fila->inicio=fila->fim;
+	fila->inicio->prox=NULL;
+	fila->tamanho=0;
+}
+
+int vazia (FILA fila){
+	return (fila.inicio==fila.fim);
+}
+
+int enqueue(ITEM item,FILA *fila){
+	PONT auxiliar;
+
+	auxiliar=(PONT)malloc(sizeof(CELULA));
+
+	auxiliar->item=item;
+	auxiliar->prox=NULL;
+
+	fila->fim->prox = auxiliar;
+	fila->fim = auxiliar;
+
+	fila->tamanho++;
+
+	printf("Item inserido na fila %d\n",item.chave);
+	return 0;
+}
+
+int look(FILA *fila,ITEM *item){
+	if (vazia(*fila))
+		return -1;
+
+	*item=fila->inicio->prox->item;
+
+	return 0;
+}
+
+void listarElementos(FILA *fila){
+	PONT aux = fila->inicio->prox;
+	printf("\n========FILA ATUAL===========\n");
+
+	while(aux !=NULL){
+        printf("|%d| -> ", aux->item.chave);
+        aux = aux->prox;
+    }
+    printf("NULL\n");
+
+}
